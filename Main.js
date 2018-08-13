8f7a5c26511d81cdbd677d4ded0324383782966d
liucol <pearl-iooi@foxmail.com>
2018-08-13 20:41:53 +0800

    加载图片

--- Main.js
+++ Main.js
@@ -1,6 +1,9 @@
+import { ResourceLoader } from "./js/base/ResourceLoader.js";
+
 //初始化游戏的整个精灵，作为游戏的入口
 export class Main {
     constructor() {
         console.log("我执行啦")
+        new ResourceLoader
     }
 }
--- js/base/ResourceLoader.js
+++ js/base/ResourceLoader.js
@@ -1,2 +1,29 @@
+import { Resources } from "./Resources.js";
+
 //资源文件加载器，确保canvas在图片资源加载完成后才进行渲染
-export class ResourceLoader {}
+export class ResourceLoader {
+    constructor() {
+        this.map = new Map(Resources);
+        for (let [key, value] of this.map) {
+            //新建一个图片对象
+            const image = new Image();
+            //设置图片的路径
+            image.src = value;
+            //把map对象的值换成image对象
+            this.map.set(key, image);
+        }
+    }
+
+    onLoaded(callback) {
+        let loadedCount = 0;
+        for (let value of this.map.values()) {
+            value.onLoaded = () => {
+                loadedCount++;
+                if (loadedCount >= this.map.size) {
+                    callback(this.map)
+                }
+            }
+        }
+
+    }
+}
