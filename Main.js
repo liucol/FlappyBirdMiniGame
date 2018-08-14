ec70a02866d2b57df5282a5c3796cbd6079a8e3d
liucol <pearl-iooi@foxmail.com>
2018-08-14 16:52:37 +0800

    基础精灵类的封装

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 import { ResourceLoader } from "./js/base/ResourceLoader.js";
 import { Director } from "./js/Director.js";
+import { BackGround } from "./js/runtime/BackGround.js";
 
 //初始化游戏的整个精灵，作为游戏的入口
 export class Main {
@@ -12,25 +13,27 @@ export class Main {
         //不管调用多少次  只会执行一次getInstance这个函数  这个是es6的单列模式
         //Director.getInstance();
 
-        let image = new Image();
-        image.src = "../res/background.png";
-        image.onload = () => {
-            this.ctx.drawImage(
-                image,
-                0,
-                0,
-                image.width,
-                image.height,
-                0,
-                0,
-                image.width,
-                image.height
-            )
-        }
+        // let image = new Image();
+        // image.src = "../res/background.png";
+        // //图片加载完成之后调用图片绘制方法
+        // image.onload = () => {
+        //     this.ctx.drawImage(
+        //         image, //图片对象
+        //         0, //剪裁坐标
+        //         0,
+        //         image.width, //剪裁宽度
+        //         image.height,
+        //         0, //放置坐标
+        //         0,
+        //         image.width, //图片宽高
+        //         image.height
+        //     )
+        // }
 
     }
 
     onResourceFirstLoaded(map) {
-        console.log(map);
+        let background = new BackGround(this.ctx, map.get("background"));
+        background.draw();
     }
 }
--- js/base/Sprite.js
+++ js/base/Sprite.js
@@ -1,4 +1,49 @@
 //精灵的基类，负责初始化精灵加载的资源和大小以及位置
 export class Sprite {
+    //初始化
+    constructor(ctx = null,
+        img = null,
+        srcX = 0,
+        srcY = 0,
+        srcW = 0,
+        srcH = 0,
+        x = 0, y = 0,
+        width = 0, height = 0
+    ) {
+        /**
+         * img: image对象
+         * srcX：剪裁x坐标
+         * srcY：剪裁y坐标
+         * srcW：剪裁宽度
+         * srcH：剪裁高度
+         * x：图片放置x坐标
+         * y：图片放置y坐标
+         * width：要使用的宽度
+         * height: 要使用的高度
+         */
+        this.ctx = ctx;
+        this.img = img;
+        this.srcX = srcX;
+        this.srcY = srcY;
+        this.srcW = srcW;
+        this.srcH = srcH;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    draw() {
+        this.ctx.drawImage(
+            this.img,
+            this.srcX,
+            this.srcY,
+            this.srcW,
+            this.srcH,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
 
+    }
 }
